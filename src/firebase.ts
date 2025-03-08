476ed2b91688d17466a8577ae6a69731d81a99e7
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2025-03-09 04:03:44 +0530

    remove console log for firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,13 +13,6 @@ import {
   type UserCredential,
 } from "firebase/auth";
 
-console.log("FIREBASE API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
-console.log("FIREBASE AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
-console.log("FIREBASE PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
-console.log("FIREBASE STORAGE BUCKET:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
-console.log("FIREBASE MESSAGING SENDER ID:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
-console.log("FIREBASE APP ID:", import.meta.env.VITE_FIREBASE_APP_ID);
-
 
 // const firebaseConfig = {
 //   apiKey:`${import.meta.env.VITE_FIREBASE_API_KEY}`,
@@ -48,9 +41,6 @@ const firebaseConfig = {
   messagingSenderId: "798111337089",
   appId: "1:798111337089:web:ded8c2c91881459c341b80"
 };
-
-
-console.log("FIREBASE CONFIG:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
