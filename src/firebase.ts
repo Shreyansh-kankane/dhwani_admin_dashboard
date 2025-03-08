cc41e4bc68e03179f9de8641dcc57865dc797515
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2025-03-09 04:01:22 +0530

    add firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,14 +13,44 @@ import {
   type UserCredential,
 } from "firebase/auth";
 
+console.log("FIREBASE API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("FIREBASE AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log("FIREBASE PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("FIREBASE STORAGE BUCKET:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log("FIREBASE MESSAGING SENDER ID:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+console.log("FIREBASE APP ID:", import.meta.env.VITE_FIREBASE_APP_ID);
+
+
+// const firebaseConfig = {
+//   apiKey:`${import.meta.env.VITE_FIREBASE_API_KEY}`,
+//   authDomain:`${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+//   projectId:`${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
+//   storageBucket:`${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+//   messagingSenderId:`${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+//   appId:`${import.meta.env.VITE_FIREBASE_APP_ID}`,
+// }
+
+// const firebaseConfig = { 
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyBPgx9zCp5oJ23TcWghD4QKX8Ctd-vDbtg",
+  authDomain: "dhwani-657ab.firebaseapp.com",
+  projectId: "dhwani-657ab",
+  storageBucket: "dhwani-657ab.firebasestorage.app",
+  messagingSenderId: "798111337089",
+  appId: "1:798111337089:web:ded8c2c91881459c341b80"
+};
+
+
+console.log("FIREBASE CONFIG:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
